0a6169c434ed2d286bb306da4b4d6f4ea5fb6573
kankk <286454796@qq.com>
2022-05-03 11:25:56 +0800

    chore: rollup

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import typescript from '@rollup/plugin-typescript';
+import common from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+  input: 'src/index.ts',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+    // sourcemap: true,
+  },
+  plugins: [common(), resolve(), typescript({ tsconfig: './tsconfig.json' })],
+};
